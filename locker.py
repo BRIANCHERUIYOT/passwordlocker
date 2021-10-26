0d379f88656fad2656f0013ffda2900b046d5b3b
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-26 13:44:23 +0300

    adds the save and delete methods for class credential

--- locker.py
+++ locker.py
@@ -21,6 +21,36 @@ class User:
         save_user method saves user objects into user_list
         """
         User.user_list.append(self)
+        
+        
+        
+class Credential:
+    """
+    Class that generates instances of credential
+    """
+
+    credential_list =[]
+
+
+    def __init__(self, account, acnt_username, acnt_password):
+        self.account = account
+        self.acnt_username = acnt_username
+        self.acnt_password = acnt_password
+
+    def save_credential(self):
+        '''
+        save user objects into credential
+        ''' 
+        Credential.credential_list.append(self)   
+
+    def delete_credential(self):
+        '''
+        deleteletes saved contact
+        '''
+        Credential.credential_list.remove(self)
+
+    def test_save_credential(self):
+        pass
 
    
         
